04756497ab2ad91317178c2d832abc087832cb36
dominicod <dominicodonnell99@gmail.com>
2022-11-26 15:01:52 -0600

    Feat: finish challenge

--- cars-assemble/CarsAssemble.cs
+++ cars-assemble/CarsAssemble.cs
@@ -4,16 +4,29 @@ static class AssemblyLine
 {
     public static double SuccessRate(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.SuccessRate() method");
+        if (speed == 0)
+        {
+            return 0;
+        }
+        if (speed >= 1 & speed <= 4)
+        {
+            return 1;
+        }
+        if (speed >= 5 & speed <= 8)
+        {
+            return 0.90;
+        }
+
+        return speed == 9 ? 0.80 : 0.77;
     }
     
     public static double ProductionRatePerHour(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        return speed * 221 * SuccessRate(speed);
     }
 
     public static int WorkingItemsPerMinute(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+        return (int)ProductionRatePerHour(speed) / 60;
     }
 }

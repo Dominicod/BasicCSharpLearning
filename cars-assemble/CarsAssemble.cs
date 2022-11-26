e4b92e6e91926a6dae45b332024f3fa01dad96b5
dominicod <dominicodonnell99@gmail.com>
2022-11-26 15:06:03 -0600

    Refactor: shorten if statements

--- cars-assemble/CarsAssemble.cs
+++ cars-assemble/CarsAssemble.cs
@@ -5,17 +5,11 @@ static class AssemblyLine
     public static double SuccessRate(int speed)
     {
         if (speed == 0)
-        {
             return 0;
-        }
         if (speed >= 1 & speed <= 4)
-        {
             return 1;
-        }
         if (speed >= 5 & speed <= 8)
-        {
             return 0.90;
-        }
 
         return speed == 9 ? 0.80 : 0.77;
     }

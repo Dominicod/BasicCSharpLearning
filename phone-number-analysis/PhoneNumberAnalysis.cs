fec9c3ebba40c363daee89b8cea7d7282628098e
dominicod <dominicodonnell99@gmail.com>
2022-11-26 16:35:54 -0600

    Feat: finish challenge

--- phone-number-analysis/PhoneNumberAnalysis.cs
+++ phone-number-analysis/PhoneNumberAnalysis.cs
@@ -5,7 +5,7 @@ public static class PhoneNumber
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
         var splitNumber = phoneNumber.Split("-");
-        return (splitNumber[0] == "212" ? true : false, splitNumber[1] == "555" ? true : false, splitNumber[2]);
+        return (splitNumber[0] == "212", splitNumber[1] == "555", splitNumber[2]);
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)

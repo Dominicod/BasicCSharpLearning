afd0c81ac075b8eff5caf6411763a63d913a32cd
dominicod <dominicodonnell99@gmail.com>
2022-11-26 16:08:23 -0600

    Refactor: better readability with methods

--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -1,24 +1,12 @@
 using System;
 
-public static class LogAnalysis 
+public static class LogAnalysis
 {
-    public static string SubstringAfter(this string str, string subString)
-    {
-        return str.Split(subString)[1];
-    }
-    
-    public static string SubstringBetween(this string str, string arg1, string arg2)
-    {
-        return str.Split(new[] { arg1, arg2 }, StringSplitOptions.None)[1];
-    }
-    
-    public static string Message(this string str)
-    {
-        return SubstringAfter(str, ":").Trim();
-    }
-    
-    public static string LogLevel(this string str)
-    {
-        return SubstringBetween(str, "[", "]");
-    }
+    public static string SubstringAfter(this string str, string subString) => str.Split(subString)[1];
+
+    public static string SubstringBetween(this string str, string arg1, string arg2) => str.Split(new[] { arg1, arg2 }, StringSplitOptions.None)[1];
+
+    public static string Message(this string str) => SubstringAfter(str, ":").Trim();
+
+    public static string LogLevel(this string str) => SubstringBetween(str, "[", "]");
 }
